bbd56e9be4832d6b24eca4fe39e787b5a0f392df
Maurice <maurice@tcastech.com>
2021-03-17 21:17:41 +0800

    fix bug of the CustomButton in the sign-in component, cause any buttons inside of a form element will cause the form to treat the button as type='submit'

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -61,7 +61,7 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isgooglesignin>
+            <CustomButton type="button" onClick={signInWithGoogle} isgooglesignin>
               Sign in with Google
             </CustomButton>
           </div>
